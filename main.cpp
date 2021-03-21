57d9b8323c1a1f6b7d3d10383d659923dd79e7a9
Jian Furquim <furquimjian@gmail.com>
2021-03-21 17:59:08 -0300

    refac: Reconfigurando a main

--- main.cpp
+++ main.cpp
@@ -24,46 +24,58 @@ int main(int argc, char **argv)
 		exit(0);
 	}
 
-	std::vector<Node> graph; // grafo principal
-	int vert=1, adja=1, cost, temp=0; //variaveis auxiliares 
+	 // grafo principal
+	std::vector<Node> graph;
+
+	//variaveis auxiliares 
+	int vert=1, adja=1, cost, temp=0;
 	
 	ifstream iFILE(argv[1]);
 
 	Node* node = new Node();
 	node->value = 0;
 	node->visited = false;
-	graph.push_back((Node)*node); //Inserção do nodo 0 serve para o metodo busca não retornar false no indice do primeiro nodo.
+	//Inserção do nodo 0 serve para o metodo busca não retornar false no indice do primeiro nodo.
+	graph.push_back((Node)*node);
 	graph[0].adj = graph[0].adj->arv_Insere(graph[0].adj, 0, 0, 0);
 	
 	int cont=0;
 
 	while(!iFILE.eof()) 
 	{	
+		iFILE >> vert;
+		iFILE >> adja;
 		iFILE >> cost;
 		graph[0].insertElements(&graph, vert, adja, cost);
-
-		adja++;
-		if(adja == 48)
-		{
-			vert++;
-			adja=1;
-		}
 	}
 
 	iFILE.close();
 
 	std::cout << "Print Graph: "<<endl;
-	graph[0].printGraph(graph);    //método de impressão do grafo
+	//método de impressão do grafo
+	graph[0].printGraph(graph);
 	std::cout <<endl;
 
-	std::vector< std::deque<int> > vet; //Vetor de deques para salvar todos os caminhos gerados pelo caicheiro viajante
-	std::deque<int> road; //Deque para salvar os caminhos percorridos pelo caicheiro viajante
-	HashTable hash; //Tabela hash para ser usada como auxilio no caicheiro viajante
-	road.push_back(99999); //Elemento inserido para ser usado como parametro pelo searchTravelingSalesmanSpeed que salva só o melhor caminho
-	vet.push_back(road); //inserção do deque que contem o elemento
-	road.clear(); //limpa o deque
-//	graph[0].searchTravelingSalesmanSpeed(graph, hash, graph[0].adj->arv_Busca(graph[0].adj, atoi(argv[2])), graph[0].adj->arv_Busca(graph[0].adj, atoi(argv[3])), 0, &vet, road);
-	graph[0].printTravelingSalesman(vet); // printa caminhos retornados pelos caicheiro viajante, e o melhor caminho separado
+	//Vetor de deques para salvar todos os caminhos gerados pelo caicheiro viajante
+	std::vector< std::deque<int> > vet;
+	//Deque para salvar os caminhos percorridos pelo caicheiro viajante
+	std::deque<int> road;
+	//Tabela hash para ser usada como auxilio no caicheiro viajante 
+	HashTable hash;
+	//Elemento inserido para ser usado como parametro pelo searchTravelingSalesmanSpeed que salva só o melhor caminho
+	road.push_back(99999);
+	 //inserção do deque que contem o elemento
+	vet.push_back(road);
+	//limpa o deque
+	road.clear(); 
+	graph[0].searchTravelingSalesmanSpeed(
+		graph, hash, graph[0].adj->arv_Busca(graph[0].adj, atoi(argv[2])),
+		graph[0].adj->arv_Busca(graph[0].adj, atoi(argv[3])),
+		0,
+		&vet,
+		road);
+	// printa caminhos retornados pelos caicheiro viajante, e o melhor caminho separado
+	graph[0].printTravelingSalesman(vet);
 
 	vet.clear(); // limpa vetor de deques
 	road.clear();  // limpa deque 
